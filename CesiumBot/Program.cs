e1b701a9cad28c09f1ac19bfa16fddcd453d6fed
ClonerNotFound <ClonerNotFound@gmail.com>
2025-02-20 20:04:56 +0300

    Добавьте файлы проекта.

---
+++ CesiumBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using CesiumBot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+
+namespace CesiumBot
+{
+    internal class Program
+    {
+        public static DiscordClient Client { get; set; }
+        public static CommandsNextExtension Commands {  get; set; }
+        public static async Task Main()
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJson();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+            Client.Ready += ClientOnReady;
+
+            var commandsConfig = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { jsonReader.prefix },
+                EnableMentionPrefix = true,
+                EnableDms = true,
+                EnableDefaultHelp = false
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task ClientOnReady(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ CesiumBot/config/JSONReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesiumBot.config
+{
+    public class JSONReader
+    {
+        public string token {  get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJson()
+        {
+            using (StreamReader sr = new StreamReader( path: "config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                JSONStructure jsonStructure = Newtonsoft.Json.JsonConvert.DeserializeObject<JSONStructure>(json);
+
+                token = jsonStructure.token;
+                prefix = jsonStructure.prefix;
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
